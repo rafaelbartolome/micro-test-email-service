9771fd7f249c1fddd03b0ded895a784c8470a4ff
Rafael Bartolome <rafa@rafaelbartolome.es>
2018-04-24 23:22:24 +0200

    Use micro broker

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"log"
 
-	micro "github.com/micro/go-micro"
-	"github.com/micro/go-micro/broker"
-	_ "github.com/micro/go-plugins/broker/nats"
 	pb "github.com/rafaelbartolome/micro-test-user-service/proto/user"
+	micro "github.com/micro/go-micro"
 )
 
 const topic = "user.created"
+
+type Subscriber struct{}
+
+func (sub *Subscriber) Process(ctx context.Context, user *pb.User) error {
+	log.Println("Picked up a new message")
+	log.Println("Sending email to:", user.Name)
+	return nil
+}
 
 func main() {
 	srv := micro.NewService(
@@ -20,35 +26,10 @@ func main() {
 
 	srv.Init()
 
-	// Get the broker instance using our environment variables
-	pubsub := srv.Server().Options().Broker
-	//pubsub := nats.NewBroker()
-	if err := pubsub.Connect(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Subscribe to messages on the broker
-	_, err := pubsub.Subscribe(topic, func(p broker.Publication) error {
-		var user *pb.User
-		if err := json.Unmarshal(p.Message().Body, &user); err != nil {
-			return err
-		}
-		log.Println(user)
-		go sendEmail(user)
-		return nil
-	})
-
-	if err != nil {
-		log.Println(err)
-	}
+	micro.RegisterSubscriber(topic, srv.Server(), new(Subscriber))
 
 	// Run the server
 	if err := srv.Run(); err != nil {
 		log.Println(err)
 	}
-}
-
-func sendEmail(user *pb.User) error {
-	log.Println("Sending email to:", user.Name)
-	return nil
 }
